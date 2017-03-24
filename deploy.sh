5eb97ef598737edec9279af2aa63e62106d94f0b
cp-circleci <corbin@mesosphere.io>
2017-03-23 22:34:25 -0700

    testing

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 # Do Login
 
-curl -X POST -H "Content-Type: application/json" -d \'{ "uid": "$DCOS_USER", "password": "$DCOS_PASSWORD" }\' "http://$DCOS_MASTER/acs/api/v1/auth/login"
+curl -X POST -H "Content-Type: application/json" -d "\{ "uid": "$DCOS_USER", "password": "$DCOS_PASSWORD" \}" "http://$DCOS_MASTER/acs/api/v1/auth/login"
